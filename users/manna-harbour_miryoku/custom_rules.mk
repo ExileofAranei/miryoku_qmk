3ae6cc3fc35c74c467e34067fe0cc2d7ed3e6f31
ExileofAranei <41694699+ExileofAranei@users.noreply.github.com>
2026-02-20 23:25:36 +0300

    feat: corne adjustments

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,9 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+MIRYOKU_CLIPBOARD = WIN
+MIRYOKU_ALPHAS = QWERTY
+OLED_ENABLE = yes
+WPM_ENABLE = yes
+SPLIT_WPM_ENABLE = yes
+SPLIT_OLED_ENABLE = yes
