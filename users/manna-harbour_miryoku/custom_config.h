a2e2ed7f40714d862ad5c5eca5c4df95e206bf9b
ExileofAranei <41694699+ExileofAranei@users.noreply.github.com>
2026-02-24 00:05:57 +0300

    feat: copy layers to custom config

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -38,8 +38,8 @@ LAYOUT_split_3x6_3( \
 ) \
 LAYOUT_split_3x6_3( \
     KC_GRV,  K00, K01, K02, K03, K04,   K05, K06, K07, K08, K09, KC_LBRC, \
-    KC_NO,   K10, K11, K12, K13, K14,   K15, K16, K17, K18, K19, KC_RBRC, \
-    KC_LSFT, K20, K21, K22, K23, K24,   K25, K26, K27, K28, K29, KC_QUOT, \
+    KC_NO,   K10, K11, K12, K13, K14,   K15, K16, K17, K18, K19, KC_QUOT, \
+    KC_LSFT, K20, K21, K22, K23, K24,   K25, K26, K27, K28, K29, KC_RBRC, \
                        K32, K33, K34,   K35, K36, K37                     \
 )
 
@@ -50,9 +50,38 @@ LAYOUT_split_3x6_3( \
     LT(U_BUTTON,KC_Z),  ALGR_T(KC_X),      KC_C,               KC_V,               KC_B,               KC_N,              KC_M,               KC_COMM,            ALGR_T(KC_DOT),     LT(U_BUTTON,KC_SLSH), \
     U_NP,               U_NP,              LT(U_MEDIA,KC_ESC), LT(U_NAV,KC_SPC),   LT(U_MOUSE,KC_TAB), LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC),  LT(U_FUN,KC_DEL),   U_NP,               U_NP
 
-// Добавил знак вопроса, слъш и обратный слэш на правую руку
+#define MIRYOKU_LAYER_NAV \
+    TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+    KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              CW_TOGG,           KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           \
+    U_NA,              KC_ALGR,           TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    U_NA,              KC_INS,            KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            \
+    U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_BSPC,           KC_DEL,            U_NP,              U_NP
+
+#define MIRYOKU_LAYER_MOUSE \
+    TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+    KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_NU,              KC_MS_L,           KC_MS_D,           KC_MS_U,           KC_MS_R,           \
+    U_NA,              KC_ALGR,           TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  U_NA,              U_NU,              KC_WH_L,           KC_WH_D,           KC_WH_U,           KC_WH_R,           \
+    U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_BTN2,           KC_BTN1,           KC_BTN3,           U_NP,              U_NP
+
+#define MIRYOKU_LAYER_MEDIA \
+    TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              RGB_TOG,           RGB_MOD,           RGB_HUI,           RGB_SAI,           RGB_VAI,           \
+    KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_NU,              KC_MPRV,           KC_VOLD,           KC_VOLU,           KC_MNXT,           \
+    U_NA,              KC_ALGR,           TD(U_TD_U_FUN),    TD(U_TD_U_MEDIA),  U_NA,              OU_AUTO,           U_NU,              U_NU,              U_NU,              U_NU,              \
+    U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_MSTP,           KC_MPLY,           KC_MUTE,           U_NP,              U_NP
+
+#define MIRYOKU_LAYER_NUM \
+    KC_LBRC,           KC_7,              KC_8,              KC_9,              KC_RBRC,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
+    KC_SCLN,           KC_4,              KC_5,              KC_6,              KC_EQL,            U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+    KC_GRV,            KC_1,              KC_2,              KC_3,              KC_BSLS,           U_NA,              TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    KC_ALGR,           U_NA,              \
+    U_NP,              U_NP,              KC_DOT,            KC_0,              KC_MINS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
 #define MIRYOKU_LAYER_SYM \
-    KC_LCBR,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,  U_NA,    TD(U_TD_U_BASE),  TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),  TD(U_TD_BOOT), \
-    KC_COLN,  KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,  KC_QUES, KC_LSFT,          KC_LCTL,           KC_LALT,         KC_LGUI,       \
-    KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,  KC_SLSH, TD(U_TD_U_SYM),   TD(U_TD_U_MOUSE),  KC_ALGR,         KC_BSLS,          \
-    U_NP,     U_NP,    KC_LPRN, KC_RPRN, KC_UNDS,  U_NA,    U_NA,             U_NA,              U_NP,            U_NP
+    KC_LCBR,           KC_AMPR,           KC_ASTR,           KC_LPRN,           KC_RCBR,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
+    KC_COLN,           KC_DLR,            KC_PERC,           KC_CIRC,           KC_PLUS,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+    KC_TILD,           KC_EXLM,           KC_AT,             KC_HASH,           KC_PIPE,           U_NA,              TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  KC_ALGR,           U_NA,              \
+    U_NP,              U_NP,              KC_LPRN,           KC_RPRN,           KC_UNDS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+#define MIRYOKU_LAYER_FUN \
+    KC_F12,            KC_F7,             KC_F8,             KC_F9,             KC_PSCR,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
+    KC_F11,            KC_F4,             KC_F5,             KC_F6,             KC_SCRL,           U_NA,              KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+    KC_F10,            KC_F1,             KC_F2,             KC_F3,             KC_PAUS,           U_NA,              TD(U_TD_U_FUN),    TD(U_TD_U_MEDIA),  KC_ALGR,           U_NA,              \
+    U_NP,              U_NP,              KC_APP,            KC_SPC,            KC_TAB,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
